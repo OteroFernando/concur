2757b43932dbe18c26de9f69018e90de4764d198
loller <ollerr67@gmail.com>
2017-07-07 14:25:51 -0300

    arreglados algunos errores de compilacion

--- stm haskell/barrera.hs
+++ stm haskell/barrera.hs
@@ -2,28 +2,26 @@ module Main where
 import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
-import Control.Parallel
-
-
 
 
 esperar tope cont rest = do
 	atomically(do
 		a <- readTvar tope;
 		c <- readTvar cont;
-		check(tope < cont && rest = 0);
-		cont = writeTvar(c+1)
+		check(tope < cont && rest == 0);
+		cont <- writeTvar(c+1)
 		);
 	atomically(
 		do
-			check(a = c)
+			check(a == c)
 		);
 	atomically(
 		do
-			rest = writeTvar(rest+1)
-			if (rest = a) then
-				cont = writeTvar(0)
-				rest = writeTvar(0)
+			rest <- writeTvar(rest+1)
+			if (rest == a) then
+				do
+				cont <- writeTvar(0)
+				rest <- writeTvar(0)
 			else
 				idle
 		);
