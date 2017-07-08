b824a3b4324850c299742241498eefe50d8ba4ff
loller <ollerr67@gmail.com>
2017-07-08 16:17:42 -0300

    FERNANDOOOOOOOO LOS ERRORES

--- stm haskell/barrera.hs
+++ stm haskell/barrera.hs
@@ -14,12 +14,16 @@ esperar tope cont rest = do
 		}
 		);
 	atomically(
-		do
+		do{
+			a <- readTVar tope;
+			c <- readTVar cont;
 			check(a == c)
+		}
 		);
 	atomically(
 		do
 		{	
+			a <- readTVar tope;
 			if (rest == a-1) then
 				do
 				writeTVar cont (0);
@@ -33,6 +37,6 @@ esperar tope cont rest = do
 crear n = atomically(do {
 	v1 <- newTVar 0;
  	v2 <- newTVar 0;
-	esperar n v1 v2 })
+	esperar n v1 v2 });
 
 main = crear 2
