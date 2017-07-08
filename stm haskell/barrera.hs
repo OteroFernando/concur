b0e2d7f7a7ddb1f71aed7a390aeac16c1f63e2e8
loller <ollerr67@gmail.com>
2017-07-08 16:26:00 -0300

    EL IF

--- stm haskell/barrera.hs
+++ stm haskell/barrera.hs
@@ -24,19 +24,27 @@ esperar tope cont rest = do
 		do
 		{	
 			a <- readTVar tope;
-			if (rest == a-1) then
-				do
+			d <- readTVar rest
+			if d == a-1 
+				then	do{
 				writeTVar cont (0);
 				writeTVar cont (0)
-			else
-				do
+				}
+				else	do{
 				writeTVar rest (rest+1)
+				}
 		}
 		);
+
+
+
+
 
 crear n = atomically(do {
 	v1 <- newTVar 0;
  	v2 <- newTVar 0;
 	esperar n v1 v2 });
 
-main = crear 2
+main = atomically(do {
+	a <- newTVar 2;
+ 	crear a });
