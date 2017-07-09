8ced6f39ddb13a88d2bdb745f2412369dcc48106
pernoctador <fergabot@gmail.com>
2017-07-09 01:12:24 -0300

    compila

--- stm haskell/RWLock.hs
+++ stm haskell/RWLock.hs
@@ -3,57 +3,64 @@ import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
 
---quite el ejemplo xq no tenia nada q ver. era wr sin lock xD
 
+wlock :: TVar Integer -> TVar Integer -> IO ()
 wlock readCant writeCant = do
---atomically(
+	atomically(
 		do{
 			w <- readTVar writeCant;
 			check(w == 0); 	--si alguien escribe tengo q esperar
 			writeTVar writeCant (w+1)
-		}
---)
---atomically(
+		});
+		atomically(
 		do{
 			r <- readTVar readCant;
-			check(r == 0) 	--si alguien lee tengo q esperar
-		};
---);
+			check(r == 0); 	--si alguien lee tengo q esperar
+		})
 
+wunlock :: TVar Integer -> IO ()
 wunlock writeCant = do
---atomically(
+	atomically(
 		do{
 			w <- readTVar writeCant;
 			writeTVar writeCant (w-1)
-		};
---)
+		}
+		)
 
+rlock :: TVar Integer -> TVar Integer -> TVar Integer -> IO ()
 rlock readCant writeCant maxLectores = do	--maxLectores es del punto 4
---	atomically(
+	atomically(
 		do{
 			w <- readTVar writeCant;
 			r <- readTVar readCant;
 			check(w == 0); 	--si alguien escribe tengo q esperar
 			writeTVar readCant (r+1)
-		};
---);
---		atomically(	--en el punto 3 este atomically no va
+		});
+		atomically(	--en el punto 3 este atomically no va
 		do{
 			r <- readTVar readCant;
 			n <- readTVar maxLectores;
 			check(r < n); 	--si hay mas de n lectores tengo q esperar
-		};
---)
+		})
 
+runlock :: TVar Integer -> IO ()
 runlock readCant = do
---atomically(
+	atomically(
 		do{
 			r <- readTVar readCant;
 			writeTVar readCant (r-1)
-		};
---)
+		}
+		)
 
-main = atomically(do{
-	a <- newTVar 2;
-	runlock a	
-	});
+rwlock :: TVar Integer -> IO ()
+rwlock maxLectores = do {
+	v1 <- atomically(newTVar 0);
+ 	v2 <- atomically(newTVar 0);
+	rlock maxLectores v1 v2;
+	runlock v1
+}
+
+--main :: TVar Integer -> IO ()
+main = do {
+	a <- atomically(newTVar 2);
+ 	rwlock a }
--- stm haskell/barrera.hs
+++ stm haskell/barrera.hs
@@ -3,49 +3,48 @@ import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
 
-
-esperar tope cont rest = do{
---	atomically(
+esperar :: TVar Integer -> TVar Integer -> TVar Integer -> IO ()
+esperar tope cont rest = do
+	atomically(
 		do{
 			a <- readTVar tope;
 			c <- readTVar cont;
-			check(tope > cont && rest == 0);
+			r <- readTVar rest;
+			check(a > c && r == 0);
 			writeTVar cont (c+1)
-		};
---		);
---	atomically(
+		}
+		);
+	atomically(
 		do{
 			a <- readTVar tope;
 			c <- readTVar cont;
 			check(a == c)
-		};
---		);
-	--atomically(
+		}
+		);
+	atomically(
 		do
 		{	
 			a <- readTVar tope;
-			d <- readTVar rest;
-			if d == a-1 
+			r <- readTVar rest;
+			if r == a-1 
 				then	do{
 				writeTVar cont (0);
 				writeTVar cont (0)
 				}
 				else	do{
-				writeTVar rest (rest+1)
+				writeTVar rest (r+1)
 				}
-		};
---		)
-		};
+		}
+		)
 
-
-
-
-
+barrera :: TVar Integer -> IO ()
 barrera n = do {
-	v1 <- newTVar 0;
- 	v2 <- newTVar 0;
-	esperar n v1 v2 };
+	v1 <- atomically(newTVar 0);
+ 	v2 <- atomically(newTVar 0);
+	esperar n v1 v2
+}
 
-main = atomically(do {
-	a <- newTVar 2;
- 	barrera a });
+--main :: TVar Integer -> IO ()
+main = do {
+	a <- atomically(newTVar 2);
+ 	barrera a }
